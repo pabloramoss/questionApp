12f4dc916aecc1224d52a89bc03b8ce80c47ed84
pabloramoss <pablo-362@hotmail.com>
2023-07-12 10:51:23 +0200

    feat: connect with supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-const QUESTIONS = [
-  { id: '1', text: '¿How old are you?' },
-  { id: '2', text: '¿What do you do for a living?' },
-];
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://bilfodlqdkporhdmqwqv.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey!);
+
+async function getQuestions() {
+  const questions = await supabase
+    .from('questions')
+    .select()
+    .then(({ data }) => data as { id: string; text: string }[]);
+
+  return questions;
+}
 
 export default async function Home() {
+  const questions = await getQuestions();
+
   return (
     <div className="grid gap-8">
       <form className="grid gap-4">
@@ -22,8 +34,8 @@ export default async function Home() {
         </button>
       </form>
       <hr className="opacity-30" />
-      <article className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(230px,1fr))]">
-        {QUESTIONS.map((question) => (
+      <article className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(230px,1fr))] items-start">
+        {questions.map((question) => (
           <section key={question.id}>
             <p className="bg-pink-500 text-white p-4 rounded-t-lg text-xl font-medium">Ask me</p>
             <p className="bg-white text-black p-4 rounded-b-lg text-xl">{question.text}</p>
